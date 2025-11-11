f9d42799f6660ae0c82cb419e1c2d222765f607b
ElCorbacho <histopat.cor@gmail.com>
2025-11-10 22:55:10 -0300

    prueba2 test3

--- src/main/java/com/example/demo/controllers/HomeController.java
+++ src/main/java/com/example/demo/controllers/HomeController.java
@@ -1,13 +1,16 @@
 package com.example.demo.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("{/series}")    
 public class HomeController {
 
-    @GetMapping("/")
-    public String home() {
+    @GetMapping
+    public String index() {
         return "index";
     }
-
 }
+
+
